9137cb20cb32cb27c21dfe602f6225e7ea9cf9ef
Pawel <p.p.wronski@gmail.com>
2016-02-02 11:52:09 +0100

    scroll to the top, google maps

--- js/app.js
+++ js/app.js
@@ -17,14 +17,43 @@ $(function() {
         }, 1500, 'easeInOutExpo');
         event.preventDefault();
     });
-});
+
 
 // Highlight the top nav as scrolling occurs
-$('body').scrollspy({
-    target: '.navbar-fixed-top'
-});
+    $('body').scrollspy({
+        target: '.navbar-fixed-top'
+    });
 
 // Closes the Responsive Menu on Menu Item Click
-$('.navbar-collapse ul li a').click(function() {
-    $('.navbar-toggle:visible').click();
+    $('.navbar-collapse ul li a').click(function() {
+        $('.navbar-toggle:visible').click();
+
+    });
+
+});
+
+$(document).ready(function(){
+
+    function scrollMe () {
+        var offset=150; // when to show scroll button
+        $(window).scroll(function() {
+            if ($(this).scrollTop() > offset) {
+                $('.top').fadeIn(500);
+            }
+            else {
+                $('.top').fadeOut(500);
+            }
+        });
+
+        // Smooth animation when scrolling
+        $('.top').click(function(event) {
+            event.preventDefault();
+            $('html, body').animate({
+                scrollTop: 0});
+        })
+
+    }
+
+    scrollMe();
+
 });
