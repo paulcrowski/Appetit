cb7dd393e838e2791c7a8291d631db0a52a5ca37
Pawel <p.p.wronski@gmail.com>
2016-02-08 15:22:06 +0100

    3 responsive design ready, statisc page rage

--- js/app.js
+++ js/app.js
@@ -1,36 +1,11 @@
 /**
  * Created by Pol on 2015-12-31.
  */
-console.log('test');
-
-var test = function () {
-    console.log('test');
-};
-
-// scroll spy navbar changes
-// Add scrollspy to <body>
-$(function() {
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').start().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
-    });
-
-
-// Highlight the top nav as scrolling occurs
-    $('body').scrollspy({
-        target: '.navbar-fixed-top'
-    });
-
-// Closes the Responsive Menu on Menu Item Click
-    $('.navbar-collapse ul li a').click(function() {
-        $('.navbar-toggle:visible').click();
-
-    });
-
-});
+//console.log('test');
+//
+//var test = function () {
+//    console.log('test');
+//};
 
 $(document).ready(function(){
 
@@ -50,9 +25,29 @@ $(document).ready(function(){
             event.preventDefault();
             $('html, body').animate({
                 scrollTop: 0});
-        })
+        });
 
     }
+
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').start().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+
+// Highlight the top nav as scrolling occurs
+    $('body').scrollspy({
+        target: '.navbar-fixed-top'
+    });
+
+// Closes the Responsive Menu on Menu Item Click
+    $('.navbar-collapse ul li a').click(function() {
+        $('.navbar-toggle:visible').click();
+
+    });
 
     scrollMe();
 
